f163b2f14f2c35f9665b58aea7e0658581ce2edd
Sabantsev <sabancev2011@yandex.ru>
2022-04-14 15:07:11 +0300

    hiden cart popup

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,22 @@
 import { BagIcon } from '../assets';
-import { Link } from 'react-router-dom';
+import { Link, useMatch } from 'react-router-dom';
 import { RootState } from '../redux/reducers';
 import { useSelector, useDispatch } from 'react-redux';
 import { CartPopUp } from '../components';
 import { deleteCartItem, setCartPopUp } from '../redux/actions/cart';
 import { CartItemType } from '../types/cart';
+import { useEffect, useState } from 'react';
 
 const Header: React.FC = () => {
   const dispatch = useDispatch()
   const { totalQty, totalPrice, cartItems, isCartPopUp } = useSelector(({ cart }: RootState) => cart)
   const onDeleteCartItem = (cartItem: CartItemType) => dispatch(deleteCartItem(cartItem))
   const onSetCartPopUp = (isShow: boolean) => dispatch(setCartPopUp(isShow))
+  const cartLocation = useMatch("/cart")
+  const [isCartLocation, setIsCartLocation] = useState(false)
+  useEffect(() => {
+    cartLocation ? setIsCartLocation(true) : setIsCartLocation(false)
+  }, [cartLocation])
 
   return (
     <header className="header">
@@ -21,7 +27,9 @@ const Header: React.FC = () => {
             <div className="header__cart">
               <Link to="/cart"><button className="header__cartBtn">
                 <BagIcon className='header__bagIcon' />{`(${totalQty}) ITEMS`}</button></Link>
-              <CartPopUp totalPrice={totalPrice} cartItems={cartItems} onDeleteCartItem={onDeleteCartItem} isCartPopUp={isCartPopUp} onSetCartPopUp={onSetCartPopUp} />
+              {!isCartLocation &&
+                <CartPopUp totalPrice={totalPrice} cartItems={cartItems} onDeleteCartItem={onDeleteCartItem} isCartPopUp={isCartPopUp} onSetCartPopUp={onSetCartPopUp} />
+              }
             </div>
             :
             <div className="header__cart">
