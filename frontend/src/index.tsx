62b2a820ed7270e2eb127f7abf83926537c9afee
SabantsevES <SabantsevES@msk.bcs.ru>
2023-07-30 22:21:26 +0300

    fixing project structure

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import App from './App';
 import './scss/App.scss';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container!);
+
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
         <App />
       </Provider>
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
--- frontend/src/redux/actions/items.ts
+++ frontend/src/redux/actions/items.ts
@@ -6,9 +6,9 @@ export const setItems = (items: Item[]) => ({ type: ItemsActionTypes.SET_ITEMS, 
 export const setFilter = (filter: FilterType) => ({ type: ItemsActionTypes.SET_FILTER, payload: filter })
 export const setSort = (sort: SortType) => ({ type: ItemsActionTypes.SET_SORT, payload: sort })
 
-export const fetchItems = (filter: FilterType, sort: SortType) => {
+export const fetchItems = (filter: FilterType, sort: SortType): any => {
     return async (dispatch: Dispatch<ItemsActions>) => {
-        const { data } = await axios.get<Item[]>(`/items?${filter !== 'all' ? `type=${filter}` : ''}${sort !== 'default' ? `&_sort=${sort}&_order=asc` : ''}`);
+        const { data } = await axios.get<Item[]>(`http://${process.env.REACT_APP_API_URL}/items?${filter !== 'all' ? `type=${filter}` : ''}${sort !== 'default' ? `&_sort=${sort}&_order=asc` : ''}`);
         dispatch({ type: ItemsActionTypes.SET_ITEMS, payload: data })
     }
 }
